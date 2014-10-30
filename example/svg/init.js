2fd0d9168f359531bbce1fb3c9f4736273e29941
yiminghe <yiminghe@gmail.com>
2014-10-30 12:59:04 +0800

    support strike-dash anim

--- example/svg/init.js
+++ example/svg/init.js
@@ -1,4 +1,8 @@
 /** @jsx React.DOM */
 
-var Line  = require('./line');
-React.render(<Line start={{x: 20, y: 20}} end={{x: 300, y: 300}}/>, document.body);
+var Line = require('./line');
+var Dash = require('./strike-dash');
+React.render(<div>
+    <Line start={{x: 20, y: 20}} end={{x: 300, y: 300}} duration={2}/>
+    <Dash start={{x: 200, y: 200}} end={{x: 300, y: 300}} duration={2}/>
+</div>, document.body);
--- example/svg/line.js
+++ example/svg/line.js
@@ -22,7 +22,7 @@ module.exports = React.createClass({
             y: end.y
         }, {
             easing: "swing",
-            duration: 5,
+            duration: this.props.duration,
             frame: function (anim, fx) {
                 current[fx.prop] = fx.val;
                 var path = 'M' + start.x + ' ' + start.y + ' L' + current.x + ' ' + current.y;
@@ -44,7 +44,10 @@ module.exports = React.createClass({
             end = <circle cx={this.props.end.x} cy={this.props.end.y} r="10" stroke="black"
             strokeWidth="5" fill="red"/>;
         }
+        var x = 'js anim';
         return (
+            <div>
+                <div dangerouslySetInnerHTML={{__html: x}}></div>
             <svg width="400" height="400">
                 <circle cx={this.props.start.x} cy={this.props.start.y} r="10" stroke="black"
                 strokeWidth="5" fill="red"/>
@@ -54,6 +57,7 @@ module.exports = React.createClass({
                 strokeWidth="2"
                 ></path>
             </svg>
+                </div>
             );
     }
 
