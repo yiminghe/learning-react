a2140dc9648a66d45ec96c7c21a8e4ff8619c45f
yiminghe <yiminghe@gmail.com>
2014-10-31 13:34:18 +0800

    add css transition group demo

---
+++ example/css-transition-group/init.js
@@ -0,0 +1,38 @@
+/** @jsx React.DOM */
+
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var TodoList = React.createClass({
+    getInitialState: function () {
+        return {items: ['hello', 'world', 'click', 'me']};
+    },
+    handleAdd: function () {
+        var newItems =
+            this.state.items.concat([prompt('Enter some text')]);
+        this.setState({items: newItems});
+    },
+    handleRemove: function (i) {
+        var newItems = this.state.items;
+        newItems.splice(i, 1);
+        this.setState({items: newItems});
+    },
+    render: function () {
+        var items = this.state.items.map(function (item, i) {
+            return (
+                <div key={item} onClick={this.handleRemove.bind(this, i)}>
+          {item}
+                </div>
+                );
+        }.bind(this));
+        return (
+            <div>
+                <button onClick={this.handleAdd}>Add Item</button>
+                <ReactCSSTransitionGroup transitionName="example">
+          {items}
+                </ReactCSSTransitionGroup>
+            </div>
+            );
+    }
+});
+
+React.render(<TodoList />, document.body);
