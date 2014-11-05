7586eb304725872125886b37935ecd2618a15967
yiminghe <yiminghe@gmail.com>
2014-11-05 17:27:51 +0800

    update demo

--- example/css-transition-group/init.js
+++ example/css-transition-group/init.js
@@ -1,6 +1,6 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var React = require('react/addons');
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var TodoList = React.createClass({
