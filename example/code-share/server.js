284e2aecb0ee31872d4cb251c82ee1f7e8e03f95
yiminghe <yiminghe@gmail.com>
2014-11-06 12:56:08 +0800

    update code-share demo

--- example/code-share/server.js
+++ example/code-share/server.js
@@ -2,17 +2,13 @@
 
 var React = require('react');
 var Component = require('./component');
-var tpl = require('fs').readFileSync(require('path').join(__dirname,'tpl.html'),{
-    encoding:'utf-8'
+var tpl = require('fs').readFileSync(require('path').join(__dirname, 'tpl.html'), {
+    encoding: 'utf-8'
 });
 module.exports = function (app) {
-    app.get('/example/code-share',function *(next){
-        if(!this.url.endsWith('/')){
-            this.redirect('/example/code-share/');
-        } else{
-            var count=10;
-            this.body = tpl.replace(/{content}/, React.renderToString(<Component count={count}/>))
-                .replace(/{count}/, count);
-        }
+    app.get('/example/code-share/demo.html', function *(next) {
+        var count = 10;
+        this.body = tpl.replace(/\{content\}/, React.renderToString(<Component count={count}/>))
+            .replace(/\{count\}/, count);
     });
 };
