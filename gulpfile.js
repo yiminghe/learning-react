e9433e7ae469121a32a07739096a11ea4ea5f7df
yiminghe <yiminghe@gmail.com>
2014-12-05 03:58:40 +0800

    upgrade modulex

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,3 @@ var gulp = require('gulp');
 var fs = require('fs');
 var path = require('path');
 var cwd = process.cwd();
-
-gulp.task('config', function () {
-  var qsIndex = path.resolve(cwd, 'node_modules/react-router/node_modules/qs/index.js');
-  if (fs.existsSync(qsIndex)) {
-    fs.writeFileSync(qsIndex, "/*modified*/module.exports = require('./lib/');");
-  }
-  var modulexNpm = require('modulex-npm');
-  var config = modulexNpm.generateConfig(['react', 'react-router', 'jquery', 'flux', 'events','object.assign']);
-  fs.writeFileSync(path.join(cwd, 'config.js'), 'require.config(' + JSON.stringify(config, null, 4) + ');');
-});
