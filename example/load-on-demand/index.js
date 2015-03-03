5bd7776c2cb75da2f83eb031160a9be8ef30beda
yiminghe <yiminghe@gmail.com>
2015-03-03 22:56:53 +0800

    test __webpack_public_path__

--- example/load-on-demand/index.js
+++ example/load-on-demand/index.js
@@ -1,6 +1,8 @@
 /** @jsx React.DOM */
 
 require('./index.css');
+// dynamic set __webpack_public_path__
+__webpack_public_path__ = '/build/example/';
 var React = require('react');
 function load() {
   require(['./async'], function (Async) {
