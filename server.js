cd402da855b8d8d87d8574a4bfa93cd6fa523ec0
yiminghe <yiminghe@gmail.com>
2015-05-29 00:16:41 +0800

    update test cover

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ require('babel/register')({
   extensions: ['.jsx']
 });
 
+var babel = require('babel');
 var router = require('koa-router');
 var webpack = require('webpack');
 var app = require('koa')();
+var fs = require('fs');
 app.use(router(app));
-app.use(require('koa-webpack-dev-test-server').middleware());
+app.use(require('koa-webpack-dev-test-server').middleware({
+  nodeJscover: {
+    // babel modify line mapping...
+    originalFileLoader: function (ctx, srcPath) {
+      var code = fs.readFileSync(srcPath, {
+        encoding: 'utf-8'
+      });
+      try {
+        ctx.body = babel.transform(code).code;
+      } catch (e) {
+        console.log(e);
+        ctx.body = e;
+      }
+    }
+  }
+}));
 var port = 8000;
 require('./example/code-share/server')(app);
 app.listen(port);
