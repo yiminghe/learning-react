2f07c12fc1784c8e5eb68fa65df6909cdd250b91
yiminghe <yiminghe@gmail.com>
2015-07-03 22:58:07 +0800

    update gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
 sh ./build.sh
 rm -rf dist
 mkdir dist
 cp -r build dist/build
 cp -r example dist/example
-gh-pages -d dist
+cp -r tutorial dist/tutorial
+./node_modules/.bin/replace '/node_modules/reveal.js/' '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.1.0/' dist/tutorial -r --include="*.html"
+#gh-pages -d dist
