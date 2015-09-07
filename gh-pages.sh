89b356be02f6d8e166163798cf0fec8002c888bf
yiminghe <yiminghe@gmail.com>
2015-09-07 14:13:46 +0800

    update reveal

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,5 +5,5 @@ mkdir dist
 cp -r build dist/build
 cp -r example dist/example
 cp -r tutorial dist/tutorial
-./node_modules/.bin/replace '/node_modules/reveal.js/' '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.1.0/' dist/tutorial -r --include="*.html"
+./node_modules/.bin/replace '/node_modules/reveal.js/' 'https://a.alipayobjects.com/reveal/3.1.0/' dist/tutorial -r --include="*.html"
 gh-pages -d dist
